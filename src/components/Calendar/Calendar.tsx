71b6c1902192b9ee4a083504cec2272601bc9c31
zhrk <zshchipets@gmail.com>
2022-12-20 01:24:37 +0300

    added controls

--- src/components/Calendar/Calendar.tsx
+++ src/components/Calendar/Calendar.tsx
@@ -1,3 +1,28 @@
-const Calendar = () => <div>Calendar</div>;
+import { addMonths, subMonths } from 'date-fns';
+import { useState } from 'react';
+import styles from './styles.module.scss';
+
+const Calendar = () => {
+  const [date, setDate] = useState(new Date());
+
+  const goToPrev = () => setDate((prev) => subMonths(prev, 1));
+  const goToNext = () => setDate((prev) => addMonths(prev, 1));
+
+  return (
+    <div className={styles.container}>
+      <div className={styles.top}>
+        <div className={styles.topControls}>
+          <button type="button" onClick={goToPrev}>
+            prev
+          </button>
+          <button type="button" onClick={goToNext}>
+            next
+          </button>
+        </div>
+        {date.toString()}
+      </div>
+    </div>
+  );
+};
 
 export { Calendar };
