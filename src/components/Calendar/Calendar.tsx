b1780cdb0bbb6dc4569f1d77aa5f2db6ffed2bfe
zhrk <zshchipets@gmail.com>
2023-01-05 23:31:41 +0300

    added events create

--- src/components/Calendar/Calendar.tsx
+++ src/components/Calendar/Calendar.tsx
@@ -1,11 +1,20 @@
 import clsx from 'clsx';
-import { addMonths, format, isSameMonth, subMonths } from 'date-fns';
+import { addMonths, format, isSameDay, isSameMonth, subMonths } from 'date-fns';
 import { useState } from 'react';
+import { nanoid } from 'nanoid';
 import { getDays } from './helpers';
 import styles from './styles.module.scss';
 
+type Event = {
+  id: string;
+  date: Date;
+};
+
+type Events = Event[];
+
 const Calendar = () => {
   const [date, setDate] = useState(new Date());
+  const [events, setEvents] = useState<Events>([]);
 
   const goToNow = () => setDate(new Date());
   const goToPrev = () => setDate((prev) => subMonths(prev, 1));
@@ -13,7 +22,10 @@ const Calendar = () => {
 
   const days = getDays(date);
 
+  const createEvent = (event: Event) => setEvents((prev) => [...prev, event]);
+
   // getDays проревьювить
+  // переключение месяцев колёсиком мыши
 
   return (
     <div className={styles.container}>
@@ -32,14 +44,31 @@ const Calendar = () => {
         {format(date, 'dd.MM.yyyy')}
       </div>
       <div className={styles.days}>
-        {days.map((day) => (
-          <div
-            key={day.toString()}
-            className={clsx(styles.day, !isSameMonth(day, date) && styles.notAvailable)}
-          >
-            {format(day, 'dd')}
-          </div>
-        ))}
+        {days.map((day) => {
+          const currentEvents = events.filter((event) => isSameDay(day, event.date));
+
+          const handleCreate = () => createEvent({ id: nanoid(), date: day });
+
+          return (
+            <div
+              key={day.toString()}
+              tabIndex={0}
+              role="button"
+              onClick={handleCreate}
+              onKeyDown={(event) => {
+                if (event.code === 'Space') {
+                  handleCreate();
+                }
+              }}
+              className={clsx(styles.day, !isSameMonth(day, date) && styles.notAvailable)}
+            >
+              {format(day, 'dd')}
+              {currentEvents.map((event, index) => (
+                <div key={event.id}>{`Event - ${index + 1}`}</div>
+              ))}
+            </div>
+          );
+        })}
       </div>
     </div>
   );
